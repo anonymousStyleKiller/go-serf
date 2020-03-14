e3bb46d140ea0a819181c1eac4bca246d3aab46d
Anton Kharchenko <gumb1t97@gmail.com>
2020-03-14 12:40:02 +0300

    Block sleep

--- js/main.js
+++ js/main.js
@@ -1,38 +1,81 @@
 $(function () {
-  $('.header__slider').slick({
-    infinite: true,
-    fade: true,
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-    asNavFor: '.slider-dots__head'
-  })
-  $('.slider-dots__head').slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    asNavFor: '.header__slider'
-  });
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots__head'
+    })
+    $('.slider-dots__head').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    });
 
-  $('.surf-slider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-    asNavFor: '.slider-map'
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-map'
 
-  });
-  $('.slider-map').slick({
-    slidesToShow: 8,
-    slidesToScroll: 1,
-    arrows: false,
-    asNavFor: '.surf-slider',
-    focusOnSelect: true
-  });
-  $('.holder__slider').slick({
-    infinite: true,
-    fade: true,
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-    // asNavFor: '.surf-slider',
-    // focusOnSelect: true
-  });
+    });
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
+    });
+    $('.holder__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        // asNavFor: '.surf-slider',
+        // focusOnSelect: true
+    });
+});
+
+$('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
+$('.quantity').each(function () {
+    var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+    btnUp.click(function () {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+
+
+    $('.quantity-button').on('click', function () {
+      let sum = $('.sum');
+      let summ = $('.nights').val() * sum.data('nights') + ($(".guests").val() - 1) * sum.data('nights');
+      sum.html('$' + summ);
+    });
+
+
+
 });
